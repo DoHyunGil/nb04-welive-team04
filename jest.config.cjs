fab9d5a99f3cbc29a7261a4bc6e809ba0fc1932d
DoHyun <kyu7085@gmail.com>
2025-12-02 11:06:49 +0900

    feat: jest 환경 설정

---
+++ jest.config.cjs
@@ -0,0 +1,21 @@
+// jest.config.cjs
+
+/** @type {import("jest").Config} **/
+const config = {
+  // 👈 ts-jest의 ESM 프리셋 사용을 명확히 지정
+  preset: 'ts-jest/presets/default-esm',
+
+  testEnvironment: 'node',
+
+  // 이전에 추가했던 설정들은 유지합니다.
+  moduleNameMapper: {
+    // .js 확장자를 .ts로 매핑하여 모듈 로드 오류 해결
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transformIgnorePatterns: [
+    // node_modules만 무시하고 나머지는 ts-jest가 변환하도록 강제
+    '/node_modules/',
+  ],
+};
+
+module.exports = config;
---
+++ src/auth/controllers/__tests/auth.controller.test.ts
@@ -0,0 +1,8 @@
+// import { prisma } from '../../../lib/prisma.js';
+
+// test('GET /detail', async () => {
+//   const data = await prisma.test.create({ data: { name: 'TEST' } });
+//   expect(data.name).toEqual('TEST');
+// });
+
+//예시로 쓰라고 남겨둔 더미 파일이에요
