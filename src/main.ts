8302f570050444855268e1389d364bc729d6ec32
DoHyun <kyu7085@gmail.com>
2025-12-18 10:25:09 +0900

    fix: requet 로거 추가

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import routers from './routers/index.js';
 import dotenv from 'dotenv';
 import pollsRouter from './routers/polls.route.js';
 import { initPollsScheduler } from './polls/utils/polls.scheduler.js';
+import { requestLogger } from './middlewares/request-logger.js';
 
 dotenv.config(); // .env 파일 환경변수 적재
 const app = express();
@@ -22,6 +23,7 @@ app.use(
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(requestLogger);
 app.use(passport.initialize());
 app.use('/api/v2/polls', pollsRouter);
 app.use('/api/v2/auth', routers.authRouter);
