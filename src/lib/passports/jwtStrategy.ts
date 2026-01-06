d0616970ecd0d5838bc87184b68089457b386958
예찬 주 <jooyc135@DESKTOP-6DCVU9O.localdomain>
2026-01-06 18:26:11 +0900

    apartment test코드 작성

--- src/lib/passports/jwtStrategy.ts
+++ src/lib/passports/jwtStrategy.ts
@@ -1,5 +1,9 @@
 import TOKEN from '../constants/jwt.tokens.js';
-import { Strategy as JwtStrategy, type VerifiedCallback, ExtractJwt } from 'passport-jwt';
+import {
+  Strategy as JwtStrategy,
+  type VerifiedCallback,
+  ExtractJwt,
+} from 'passport-jwt';
 import { prisma } from '../prisma.js';
 import type { Request } from 'express';
 
@@ -24,12 +28,12 @@ const cookieAndHeaderExtractor = (cookieName: string) => {
 
 const accessTokenOptions = {
   jwtFromRequest: cookieAndHeaderExtractor(TOKEN.ACCESS_TOKEN_COOKIE_NAME),
-  secretOrKey: TOKEN.JWT_ACCESS_TOKEN_SECRET,
+  secretOrKey: TOKEN.JWT_ACCESS_TOKEN_SECRET!,
 };
 
 const refreshTokenOptions = {
   jwtFromRequest: cookieAndHeaderExtractor(TOKEN.REFRESH_TOKEN_COOKIE_NAME),
-  secretOrKey: TOKEN.JWT_REFRESH_TOKEN_SECRET,
+  secretOrKey: TOKEN.JWT_REFRESH_TOKEN_SECRET!,
 };
 
 async function jwtVerify(payload: { userId: number }, done: VerifiedCallback) {
